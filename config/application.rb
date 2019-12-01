e5f021e4b13d624d9e89ddd2297108f1e8f7a609
kana0313 <kokusitaisaku.kana@gmail.com>
2019-12-01 20:16:38 +0900

    menber追加

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module ChatSpace
     end
     
     config.time_zone = "Tokyo"
+    # 日本時間にするやつ↑
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
